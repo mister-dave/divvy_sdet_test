87e2e72064bb96d83e721ecd3d496e4a2fce71bc
david <dtolman1@gmail.com>
2020-08-31 02:06:17 -0600

    facebook.com login page UI tests

---
+++ elixir_sdet_exercise/lib/helpers.ex
@@ -0,0 +1,52 @@
+defmodule Helpers do
+  use Hound.Helpers
+
+  @spec assign_selectors :: %{
+    confirm_email: Hound.Element.t(),
+    dob_day: Hound.Element.t(),
+    dob_month: Hound.Element.t(),
+    dob_year: Hound.Element.t(),
+    email_address: Hound.Element.t(),
+    first_name: Hound.Element.t(),
+    gender_custom: any,
+    gender_female: any,
+    gender_male: any,
+    last_name: Hound.Element.t(),
+    password: Hound.Element.t(),
+    sign_up_button: Hound.Element.t()
+  }
+
+  def assign_selectors() do
+    # flaky; next time FB runs their minifier, this will change;
+    find_element(:id, "u_0_2") |> click()
+    first_name_selector = find_element(:name, "firstname")
+    last_name_selector = find_element(:name, "lastname")
+    email_selector = find_element(:name, "reg_email__")
+    confirm_email_selector = find_element(:name, "reg_email_confirmation__")
+    password_selector = find_element(:name, "reg_passwd__")
+    dob_month_selector = find_element(:name, "birthday_month")
+    dob_day_selector = find_element(:name, "birthday_day")
+    dob_year_selector = find_element(:name, "birthday_year")
+    gender_female_selector =
+      find_all_elements(:tag, "label") |> Enum.find(&(inner_text(&1) == "Female"))
+    gender_male_selector =
+      find_all_elements(:tag, "label") |> Enum.find(&(inner_text(&1) == "Male"))
+    gender_custom_selector =
+      find_all_elements(:tag, "label") |> Enum.find(&(inner_text(&1) == "Custom"))
+    sign_up_button_selector = find_element(:name, "websubmit")
+    %{
+      first_name: first_name_selector,
+      last_name: last_name_selector,
+      email_address: email_selector,
+      confirm_email: confirm_email_selector,
+      password: password_selector,
+      dob_month: dob_month_selector,
+      dob_day: dob_day_selector,
+      dob_year: dob_year_selector,
+      gender_female: gender_female_selector,
+      gender_male: gender_male_selector,
+      gender_custom: gender_custom_selector,
+      sign_up_button: sign_up_button_selector
+    }
+  end
+end
--- elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
+++ elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
@@ -1,13 +1,219 @@
 defmodule ElixirSdetExerciseTest do
-  # Import helpers
   use Hound.Helpers
+  # use Hound.Matchers
   use ExUnit.Case
+  alias Helpers
 
-  # Start hound session and destroy when tests are run
-  hound_session()
-
-  test "goes to google" do
-    navigate_to "http://google.com"
-    IO.inspect page_title()
+  setup do
+    Hound.start_session()
+    :ok
   end
+
+  def screenshot(name) do
+    screenshot_directory = File.mkdir_p("./support/screenshots")
+    take_screenshot("./support/screenshots/#{name}_#{DateTime.utc_now()}.png")
+  end
+
+  @tag :skip
+  test "Goes to Facebook Login page" do
+    navigate_to("https://facebook.com")
+    IO.inspect(page_title())
+  end
+
+  test "Doesn't allow naughty words in name", context do
+    navigate_to("https://facebook.com")
+    fields_map = Helpers.assign_selectors()
+
+    fill_field(fields_map[:first_name], "crap")
+    fill_field(fields_map[:last_name], "crap")
+    fill_field(fields_map[:email_address], "bob556677@yahoo.com")
+    fill_field(fields_map[:confirm_email], "bob556677@yahoo.com")
+    fill_field(fields_map[:password], "UIG79haeg3x")
+
+    find_element(:css, "#month option[value='7']") |> click()
+    find_element(:css, "#day option[value='23']") |> click()
+    find_element(:css, "#year  option[value='1980']") |> click()
+
+    click(fields_map[:gender_male])
+    click(fields_map[:sign_up_button])
+    :timer.sleep(1500)
+
+    try do
+      warning_text =
+        "We require everyone to use the name they use in everyday life, what their friends call them, on Facebook. Learn more about our name policies."
+
+      assert visible_in_element?({:id, "reg_error_inner"}, ~r/#{warning_text}/)
+    catch
+      error ->
+        Map.get(context, :test)
+        |> to_string()
+        |> screenshot()
+
+        IO.inspect(error)
+    end
+  end
+
+  test "Doesn't allow symbols in names", context do
+    navigate_to("https://facebook.com")
+    fields_map = Helpers.assign_selectors()
+
+    fill_field(fields_map[:first_name], "as;lkhasel;h")
+    fill_field(fields_map[:last_name], "sal;fj!wl;ekr")
+    fill_field(fields_map[:email_address], "bob556677@yahoo.com")
+    fill_field(fields_map[:confirm_email], "bob556677@yahoo.com")
+    fill_field(fields_map[:password], "UIG79haeg3x")
+    find_element(:css, "#month option[value='7']") |> click()
+    find_element(:css, "#day option[value='23']") |> click()
+    find_element(:css, "#year  option[value='1980']") |> click()
+
+    click(fields_map[:gender_male])
+    click(fields_map[:sign_up_button])
+    :timer.sleep(1500)
+
+    try do
+      warning_text =
+        "This name has certain characters that aren't allowed. Learn more about our name policies."
+
+      assert visible_in_element?({:id, "reg_error_inner"}, ~r/#{warning_text}/)
+    catch
+      error ->
+        Map.get(context, :test)
+        |> to_string()
+        |> screenshot()
+
+        IO.inspect(error)
+    end
+  end
+
+  test "Doesn't allow long names", context do
+    navigate_to("https://facebook.com")
+    fields_map = Helpers.assign_selectors()
+
+    fill_field(fields_map[:first_name], "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
+    fill_field(fields_map[:last_name], "abcdefghijklmnopqrstuvwxyz")
+    fill_field(fields_map[:email_address], "bob556677@yahoo.com")
+    fill_field(fields_map[:confirm_email], "bob556677@yahoo.com")
+    fill_field(fields_map[:password], "UIG79haeg3x")
+    find_element(:css, "#month option[value='7']") |> click()
+    find_element(:css, "#day option[value='23']") |> click()
+    find_element(:css, "#year  option[value='1980']") |> click()
+
+    click(fields_map[:gender_male])
+    click(fields_map[:sign_up_button])
+    :timer.sleep(1500)
+
+    try do
+      warning_text =
+        "First or last names on Facebook can't have too many characters. Learn more about our name policies."
+
+      assert visible_in_element?({:id, "reg_error_inner"}, ~r/#{warning_text}/)
+    catch
+      error ->
+        Map.get(context, :test)
+        |> to_string()
+        |> screenshot()
+
+        IO.inspect(error)
+    end
+  end
+
+  # Bobby Tables
+  test "Doesn't allow injection attacks", context do
+    navigate_to("https://facebook.com")
+    fields_map = Helpers.assign_selectors()
+    fill_field(fields_map[:first_name], "Robert")
+    fill_field(fields_map[:last_name], "Tables")
+    fill_field(fields_map[:email_address], "xxx@xxx.xxx")
+    fill_field(fields_map[:confirm_email], "xxx@xxx.xxx")
+    fill_field(fields_map[:password], "xxx') OR 1 = 1 -- ]")
+    find_element(:css, "#month option[value='7']") |> click()
+    find_element(:css, "#day option[value='23']") |> click()
+    find_element(:css, "#year  option[value='1980']") |> click()
+
+    click(fields_map[:gender_male])
+    click(fields_map[:sign_up_button])
+    :timer.sleep(20000) # super long wait for this one
+
+    try do
+      warning_text =
+        "This name has certain characters that aren't allowed. Learn more about our name policies."
+
+      assert visible_in_element?({:id, "reg_error_inner"}, ~r/#{warning_text}/)
+    catch
+      error ->
+        Map.get(context, :test)
+        |> to_string()
+        |> screenshot()
+
+        IO.inspect(error)
+    end
+  end
+
+
+  test "Doesn't allow minors to register", context do
+    navigate_to("https://facebook.com")
+    fields_map = Helpers.assign_selectors()
+    fill_field(fields_map[:first_name], "Robert")
+    fill_field(fields_map[:last_name], "Tables")
+    fill_field(fields_map[:email_address], "notaminor@clubpenguin.com")
+    fill_field(fields_map[:confirm_email], "notaminor@clubpenguin.com")
+    fill_field(fields_map[:password], "CP5ever")
+    find_element(:css, "#month option[value='7']") |> click()
+    find_element(:css, "#day option[value='23']") |> click()
+    find_element(:css, "#year  option[value='2010']") |> click()
+
+    click(fields_map[:gender_male])
+    click(fields_map[:sign_up_button])
+    :timer.sleep(1500)
+
+    try do
+      warning_text =
+        "We Couldn't Create Your Account.\nWe were not able to sign you up for Facebook."
+
+      assert visible_in_element?({:id, "softblock_error_inner"}, ~r/#{warning_text}/)
+    catch
+      error ->
+        Map.get(context, :test)
+        |> to_string()
+        |> screenshot()
+
+        IO.inspect(error)
+    end
+  end
+
+  #different login page
+  # test "Visit new page after X login attempts" do
+  #   navigate_to("https://facebook.com")
+  #   page_title = page_title()
+  #   find_element(:id, "email") |> fill_field("jason@gmail.com")
+  #   find_element(:id, "pass") |> fill_field("jasonsfavoritewebsite")
+  #   find_element(:login, "login") |> click()
+
+
+  #   new_page_title = page_title()
+  #   refute page_title == new_page_title
+  # end
+  # Doesn't allow too short of a password
+  # Doesn't allow simple-sequence passwords
+  # Doesn't allow empty fields
+  # Doesn't allow invalid email address domains
+  # Doesn't allow invalid email prefix
+  # Doesn't allow mismatched email values in confirm field
+  # Doesn't allow bad words in the email address
+  # Doesn't allow name to be in password
+  # Doesn't allow date of birth to be password
+  # Doesn't allow email address to be password
+  # Doesn't allow mobile number to be password
+  # Doesn't allow many attempts in short amount of time
+  # Doesn't allow submission without first name present
+  # Doesn't allow submission without last name present
+  # Doesn't allow submission without mobile number or email address present
+  # Doesn't allow submission without date of birth present
+  # Doesn't allow submission without gender selection present
+  # Doesn't allow submission without pronoun selection present
+  # choose radio button
+  # choose other and fill field
+  # Doesn't allow submission with no values entered
+  # Doesn't allow
+  # Doesn't allow
 end
